e5028c962fa84df265c2213c4bdcb21bba59fad3
Emily <20190040@students.htl-perg.ac.at>
2022-01-21 12:07:08 +0100

    wip - gridlist

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-background',
@@ -12,4 +12,28 @@ export class BackgroundComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}*/
+
+import {Component} from '@angular/core';
+
+export interface Tile {
+  imgpath: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+ @Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.scss']
+})
+export class BackgroundComponent {
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, imgpath: 'kastlimg'},
+    {text: 'Two', cols: 1, rows: 1, imgpath: 'kastlimg'},
+    {text: 'Three', cols: 1, rows: 1, imgpath: 'kastlimg'},
+    {text: 'Four', cols: 1, rows: 1, imgpath: 'kastlimg'},
+    {text: 'Five', cols: 1, rows: 1, imgpath: 'kastlimg'}
+  ];
 }
