5865075b8617e9a383c54f0aba9382aab01c9be7
Emily <20190040@students.htl-perg.ac.at>
2022-01-28 11:43:00 +0100

    wip

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -34,6 +34,16 @@ export class BackgroundComponent {
     {text: 'Two', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
     {text: 'Three', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
     {text: 'Four', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
-    {text: 'Five', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'}
+    {text: 'Five', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Six', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Seven', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Eight', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Nine', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Ten', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Eleven', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Twelve', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Thirteen', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+    {text: 'Fourteen', cols: 1, rows: 1, imgpath: 'assets\\Images\\kastl.png'},
+
   ];
 }
