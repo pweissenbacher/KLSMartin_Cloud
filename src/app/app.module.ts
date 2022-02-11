31d942eb4b9f4182c5d530e7c8b473e835af1459
Mathias Plaimer <20190078@students.htl-perg.ac.at>
2022-02-11 11:25:20 +0100

    update toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,11 @@ import { HeaderComponent } from './header/header.component';
 import { ApplicationsComponent } from './applications/applications.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from  '@angular/material/toolbar';
-
 import { BackgroundComponent } from './background/background.component';
-
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatSelectModule } from '@angular/material/select'; 
 
 
 @NgModule({
@@ -26,6 +24,7 @@ import {MatSelectModule} from '@angular/material/select';
     BackgroundComponent,
     
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule,
