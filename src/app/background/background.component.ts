133b867f07f465cdfb7dfcf97851efd67012f0f1
Hilal <20190075@students.htl-perg.ac.at>
2022-04-29 12:17:08 +0200

    Bilder eingefügt und gelöscht/Pfade geändert

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -27,13 +27,13 @@ export class BackgroundComponent implements OnInit {
   }
 
   applications: Application[] = [
-    {id: '1', name: 'Roo', imgpath: 'assets\\Images\\roo.png'},
-    {id: '2', name: 'Tiger', imgpath: 'assets\\Images\\tiger.png'},
-    {id: '3', name: 'Poh', imgpath: 'assets\\Images\\poh.png'},
-    {id: '4', name: 'Eyyore', imgpath: 'assets\\Images\\eyyore.png'},
-    {id: '5', name: 'Piglet', imgpath: 'assets\\Images\\piglet.png'},
-    {id: '6', name: 'Junge', imgpath: 'assets\\Images\\junge.png'},
-    {id: '7', name: 'Honig', imgpath: 'assets\\Images\\honig.png'}
+    {id: '1', name: 'IPS Gate', imgpath: 'assets\\Images\\question_mark.png'},
+    {id: '2', name: 'IPS Automation Module', imgpath: 'assets\\Images\\IPSAutomation.png'},
+    {id: '3', name: 'IPS Orthognathic Module', imgpath: 'assets\\Images\\IPSOrthognathic.png'},
+    {id: '4', name: 'IPS Reconstruction Module', imgpath: 'assets\\Images\\question_mark.png'},
+    {id: '5', name: 'IPS Review Module', imgpath: 'assets\\Images\\question_mark.png'},
+    {id: '6', name: 'IPS Mission Control', imgpath: 'assets\\Images\\IPSMission.png'},
+    {id: '7', name: 'Log Gate', imgpath: 'assets\\Images\\IPSLogGate.png'}
   ]
 
   tiles: Tile[] = [
