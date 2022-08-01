275567706f48a07cb7f3abf25f299440a93c449e
Emily Stadlbauer <20190040@students.htl-perg.ac.at>
2022-08-01 11:36:44 +0200

    changed img files

--- src/app/services/applications.service.ts
+++ src/app/services/applications.service.ts
@@ -6,13 +6,13 @@ import { Application } from '../login/login.component';
 })
 export class ApplicationsService {
   private applications: Application[] = [
-    {id: '1', name: 'IPS Gate', imgpath: 'assets\\Images\\question_mark.png'},
-    {id: '2', name: 'IPS Automation Module', imgpath: 'assets\\Images\\IPSAutomationModule.png'},
-    {id: '3', name: 'IPS Orthognathic Module', imgpath: 'assets\\Images\\IPSOrthognathicModule.png'},
-    {id: '4', name: 'IPS Reconstruction Module', imgpath: 'assets\\Images\\question_mark.png'},
-    {id: '5', name: 'IPS Review Module', imgpath: 'assets\\Images\\question_mark.png'},
-    {id: '6', name: 'IPS Mission Control', imgpath: 'assets\\Images\\IPSMissionControl.png'},
-    {id: '7', name: 'Log Gate', imgpath: 'assets\\Images\\IPSLogGate.png'}
+    {id: '1', name: 'IPS Gate', imgpath: 'assets\\Images\\IPS_Gate.png'},
+    {id: '2', name: 'IPS Automation Module', imgpath: 'assets\\Images\\IPS Automation Module.png'},
+    {id: '3', name: 'IPS Orthognathic Module', imgpath: 'assets\\Images\\IPS Orthognathic Module.png'},
+    {id: '4', name: 'IPS Reconstruction Module', imgpath: 'assets\\Images\\IPS Reconstruction Module.png'},
+    {id: '5', name: 'IPS Review Module', imgpath: 'assets\\Images\\IPS Review Module.png'},
+    {id: '6', name: 'IPS Mission Control', imgpath: 'assets\\Images\\IPS Mission Control.png'},
+    {id: '7', name: 'Log Gate', imgpath: 'assets\\Images\\Log Gate.png'}
   ]
   constructor() { }
 
